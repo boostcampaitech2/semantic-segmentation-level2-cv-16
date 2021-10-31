db648b74e801223092e02205124b771d40396820
Sang-hyun Park <hyun06000@gmail.com>
2021-11-01 01:11:16 +0900

    Add SETRnet

---
+++ dev/model_develop/UNet/mmsegUNet/exp_01/mm_config/default_config.py
@@ -0,0 +1,214 @@
+# Modification
+
+pretrained_link  = "https://download.openmmlab.com/mmsegmentation"
+pretrained_link += "/v0.5/unet/pspnet_unet_s5-d16_128x128_40k_stare"
+pretrained_link += "/pspnet_unet_s5-d16_128x128_40k_stare_20201227_181818-3c2923c4.pth"
+
+
+dataset_type = "CustomDataset"
+data_root = "/tf/P_stage/P_stage_segmentation/segmentation/input/data/mmseg/"
+img_scale = (605, 700) # original scale
+crop_size = (128, 128)
+
+classes = [
+    "Background",
+    "General trash",
+    "Paper",
+    "Paper pack",
+    "Metal",
+    "Glass",
+    "Plastic",
+    "Styrofoam",
+    "Plastic bag",
+    "Battery",
+    "Clothing",
+]
+palette = [
+    [0, 0, 0],
+    [192, 0, 128],
+    [0, 128, 192],
+    [0, 128, 64],
+    [128, 0, 0],
+    [64, 0, 128],
+    [64, 0, 192],
+    [192, 128, 64],
+    [192, 192, 128],
+    [64, 64, 128],
+    [128, 0, 192],
+]
+# ==============
+
+norm_cfg = dict(type='SyncBN', requires_grad=True)
+model = dict(
+    type='EncoderDecoder',
+    pretrained=pretrained_link,
+    backbone=dict(
+        type='UNet',
+        in_channels=3,
+        base_channels=64,
+        num_stages=5,
+        strides=(1, 1, 1, 1, 1),
+        enc_num_convs=(2, 2, 2, 2, 2),
+        dec_num_convs=(2, 2, 2, 2),
+        downsamples=(True, True, True, True),
+        enc_dilations=(1, 1, 1, 1, 1),
+        dec_dilations=(1, 1, 1, 1),
+        with_cp=False,
+        conv_cfg=None,
+        norm_cfg=norm_cfg,
+        act_cfg=dict(type='ReLU'),
+        upsample_cfg=dict(type='InterpConv'),
+        norm_eval=False),
+    decode_head=dict(
+        type='PSPHead',
+        in_channels=64,
+        in_index=4,
+        channels=16,
+        pool_scales=(1, 2, 3, 6),
+        dropout_ratio=0.1,
+        num_classes=11,
+        norm_cfg=norm_cfg,
+        align_corners=False,
+        loss_decode=dict(
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)),
+    auxiliary_head=dict(
+        type='FCNHead',
+        in_channels=128,
+        in_index=3,
+        channels=64,
+        num_convs=1,
+        concat_input=False,
+        dropout_ratio=0.1,
+        num_classes=11,
+        norm_cfg=norm_cfg,
+        align_corners=False,
+        loss_decode=dict(
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=0.4)),
+    train_cfg=dict(),
+    test_cfg=dict(mode='slide', crop_size=(128, 128), stride=(85, 85))
+)
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations'),
+    dict(type='Resize', img_scale=img_scale, ratio_range=(0.5, 2.0)),
+    dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
+    dict(type='RandomFlip', prob=0.5),
+    dict(type='PhotoMetricDistortion'),
+    dict(
+        type='Normalize',
+        mean=[123.675, 116.28, 103.53],
+        std=[58.395, 57.12, 57.375],
+        to_rgb=True),
+    dict(type='Pad', size=crop_size, pad_val=0, seg_pad_val=255),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_semantic_seg'])
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=img_scale,
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(
+                type='Normalize',
+                mean=[123.675, 116.28, 103.53],
+                std=[58.395, 57.12, 57.375],
+                to_rgb=True),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img'])
+        ])
+]
+data = dict(
+    samples_per_gpu=4,
+    workers_per_gpu=4,
+    train=dict(
+        classes=classes,
+        palette=palette,
+        type=dataset_type,
+        reduce_zero_label=False,
+        img_dir=data_root + "images/training",
+        ann_dir=data_root + "annotations/training",
+        pipeline=train_pipeline,
+    ),
+    val=dict(
+        classes=classes,
+        palette=palette,
+        type=dataset_type,
+        reduce_zero_label=False,
+        img_dir=data_root + "images/validation",
+        ann_dir=data_root + "annotations/validation",
+        pipeline=test_pipeline,
+    ),
+    test=dict(
+        classes=classes,
+        palette=palette,
+        type=dataset_type,
+        reduce_zero_label=False,
+        img_dir=data_root + "images/test",
+        pipeline=test_pipeline,
+    )
+)
+# yapf:disable
+config_path  = "/tf/P_stage/P_stage_segmentation/segmentation/semantic-segmentation-level2-cv-16/dev/model_develop/UNet/mmsegUNet/exp_01/mm_config/"
+log_config = dict(
+    by_epoch=False, interval=100,
+    hooks=[
+        dict(type="TextLoggerHook"),
+        dict(
+            type="WandbLoggerHook",
+            init_kwargs=dict(
+                project="mmsegmentation",
+                name="tmp-pspnet_unet",
+                entity="sang-hyun",
+            ),
+            config_path=config_path,
+        ),
+    ],
+)
+# yapf:enable
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+load_from = pretrained_link
+resume_from = None
+workflow = [('train', 1),('val', 1)]
+cudnn_benchmark = True
+# AdamW optimizer, no weight decay for position embedding & layer norm
+# in backbone
+optimizer = dict(
+    # _delete_=True,
+    type='AdamW',
+    lr=0.00006,
+    betas=(0.9, 0.999),
+    weight_decay=0.01,
+    paramwise_cfg=dict(
+        custom_keys={
+            'absolute_pos_embed': dict(decay_mult=0.),
+            'relative_position_bias_table': dict(decay_mult=0.),
+            'norm': dict(decay_mult=0.)
+        }
+    )
+)
+optimizer_config = dict()
+lr_config = dict(
+    # _delete_=True,
+    policy='poly',
+    warmup='linear',
+    warmup_iters=1500,
+    warmup_ratio=1e-6,
+    power=1.0,
+    min_lr=0.0,
+    by_epoch=False
+)
+runner = dict(type="EpochBasedRunner", max_epochs=5000)
+checkpoint_config = dict(max_keep_ckpts=2, by_epoch=True, interval=1)
+evaluation = dict(
+    metric=['mDice',"mIoU"], 
+    interval=1, by_epoch=True,
+    pre_eval=True,
+    save_best="mIoU",
+)
