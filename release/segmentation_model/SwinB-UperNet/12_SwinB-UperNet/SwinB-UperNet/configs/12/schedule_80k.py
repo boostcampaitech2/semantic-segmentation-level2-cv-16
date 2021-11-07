db0ab4aad9137399201f8c3c6043503783b75298
ashhyun <youkind98@gmail.com>
2021-11-07 13:52:32 +0000

    update

--- release/segmentation_model/SwinB-UperNet/12_SwinB-UperNet/SwinB-UperNet/configs/12/schedule_80k.py
+++
@@ -1,9 +0,0 @@
-# optimizer
-optimizer = dict(type="AdamW", lr=1e-4, weight_decay=0.0005)
-optimizer_config = dict()
-# learning policy
-lr_config = dict(policy="poly", power=0.9, min_lr=1e-5, by_epoch=True)
-# runtime settings
-runner = dict(type="EpochBasedRunner", max_epochs=50)
-checkpoint_config = dict(max_keep_ckpts=2, by_epoch=True, interval=1)
-evaluation = dict(interval=1, metric="mIoU", pre_eval=True)
--- release/segmentation_model/SwinB-UperNet/48_SwinL-UperNet/48.py
+++ release/segmentation_model/SwinB-UperNet/48_SwinL-UperNet/48.py
@@ -1,26 +1,24 @@
-# ocrnet_hr48_512x512_40k_voc12aug
 _base_ = [
     "upernet_swin.py",
     "cocotrash.py",
     "default_runtime.py",
     "schedule_160k.py",
-    # "pretrain_384x384_1K.py",
 ]
 
 model = dict(
-    pretrained="/opt/ml/segmentation/semantic-segmentation-level2-cv-16/dev/model_develop/mmsegmentation/configs/10/pretrain/upernet_swin_base_patch4_window12_512x512_160k_ade20k_pretrain_384x384_22K_converted.pth",
+    # pretrained="path_to_pretrained_path",
     backbone=dict(
         pretrain_img_size=384,
-        embed_dims=128,
+        embed_dims=192,
         depths=[2, 2, 18, 2],
-        num_heads=[4, 8, 16, 32],
+        num_heads=[6, 12, 24, 48],
         window_size=12,
         use_abs_pos_embed=False,
         drop_path_rate=0.3,
         patch_norm=True,
     ),
-    decode_head=dict(in_channels=[128, 256, 512, 1024], num_classes=11),
-    auxiliary_head=dict(in_channels=512, num_classes=11),
+    decode_head=dict(in_channels=[192, 384, 768, 1536], num_classes=11),
+    auxiliary_head=dict(in_channels=768, num_classes=11),
 )
 
 
@@ -29,7 +27,7 @@ model = dict(
 optimizer = dict(
     _delete_=True,
     type="AdamW",
-    lr=0.00001,
+    lr=0.00006,
     betas=(0.9, 0.999),
     weight_decay=0.01,
     paramwise_cfg=dict(
@@ -37,20 +35,23 @@ optimizer = dict(
             "absolute_pos_embed": dict(decay_mult=0.0),
             "relative_position_bias_table": dict(decay_mult=0.0),
             "norm": dict(decay_mult=0.0),
+            "head": dict(lr_mult=2.0),
         }
     ),
 )
 
 lr_config = dict(
     _delete_=True,
-    policy="poly",
+    policy="CosineRestart",
+    periods=[30000, 50000],
+    restart_weights=[1.0, 0.3],
     warmup="linear",
-    warmup_iters=1500,
-    warmup_ratio=1e-6,
-    power=1.0,
-    min_lr=0.0,
-    by_epoch=True,
+    warmup_iters=500,
+    warmup_ratio=0.1,
+    min_lr=0,
+    by_epoch=False,
 )
 
 # By default, models are trained on 8 GPUs with 2 images per GPU
-data = dict(samples_per_gpu=8)
+data = dict(samples_per_gpu=4)
+seed = 16
